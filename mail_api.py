14d84f9f604d9a472f08fc55517ce7253d4640bb
c88tm <c88tm159357@gmail.com>
2018-07-04 19:02:40 +0800

    add some config

--- mail_api.py
+++ mail_api.py
@@ -8,9 +8,9 @@ from email.header import Header
 class MailSender(object):
     def __init__ (self,mail_list):
         self.mail_host= "smtp.gmail.com"  
-        self.mail_user= "6@gmail.com" # Gmail 信箱
-        self.mail_pass= "" # 密碼
-        self.sender = '36@gmail.com' # 信箱名
+        self.mail_user= "qpudding1023@gmail.com" # Gmail 信箱
+        self.mail_pass= "33554432pudding" # 密碼
+        self.sender = 'qpudding1023@gmail.com' # 信箱名
         self.receivers = mail_list
     def send(self):
         try:
@@ -22,12 +22,13 @@ class MailSender(object):
             print "成功登入"
             smtpObj.sendmail(self.sender, self.receivers, self.message.as_string())
             print "恭喜您成功寫完聯絡簿"
-        except smtplib.SMTPException:
+        except smtplib.SMTPException as e:
+            print str(e)
             print "嗚嗚 聯絡簿發不出去QAQ"
     
     def write_message(self,mail_msg,date):
         self.message = MIMEText(mail_msg, 'plain', 'utf-8')
-        self.message['From'] = Header("陳佳佑", 'utf-8')
+        self.message['From'] = Header("蔡毓聰", 'utf-8')
         self.message['To'] =  Header("學生家長", 'utf-8')
-        subject = date+' 陳佳佑班課程進度及學生學習狀況'
+        subject = date+' 蔡毓聰班課程進度及學生學習狀況'
         self.message['Subject'] = Header(subject, 'utf-8')
--- main.py
+++ main.py
@@ -1,105 +1,53 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 from mail_api import MailSender
+import ConfigParser
 msg_template ="""
 親愛的家長您好：
  
-我是資訊奧林匹亞G7班老師陳佳佑。
+我是資訊奧林匹亞G8班老師蔡毓聰。
 每次課程進度和學生學習狀況寄送給您，讓您了解貴子弟在奧林匹亞班學習狀況。
-{specialword}
 
-學生：{person[name]}
-本週上課進度：{study}
-本週小考： 無
-本週作業：{homework}
+學生：{a[name]}
+本週上課進度：{a[study]}
+本週小考： {a[quiz]}
+本週作業：{a[homework]}
 出席狀況：準時
-上課態度：{person[attitude]}
-學習狀況：{person[studycase]}
+上課態度：{a[attitude]}
+學習狀況：{a[studycase]}
 特殊狀況報告：無
 
+{a[specialword]}
 
 敬祝
 順心如意
 
-佳佑 敬上
+毓聰 敬上
 """
 
-students = [
-    {
-        'name':'謝知我',
-        'studycase':'優良',
-        'attitude':'優良',
-        'email':['philip20336@gmail.com','b04611015@csie.ntu.edu.tw']},
-    
-    {
-        'name' : '蔡嘉恩',
-        'studycase':'優良',
-        'attitude':'優良',
-        'email':['b04611015@csie.ntu.edu.tw']},
-    
-    {
-        'name' : '孫詠灝',
-        'studycase':'優良',
-        'attitude':'優良',
-        'email':['b04611015@csie.ntu.edu.tw']}
-    ]
-
-
-homework =raw_input('請輸入作業: ')
-study = raw_input('請輸入課程進度: ')
-date = raw_input('日期: ')
-for student in students:
-    print "給 "+student['name']+" 的家長"
-    special_word = ""
-
-    while True:
-        ans = raw_input('有沒有特別想對 '+student['name']+ ' 的家長說的話(y/n): ')
-        if ans == "y":
-            special_word=raw_input('請輸入想對家長說的話: ')
-            print "輸入完成"
-            break
-        if ans == "n":
-            break
-        print "請輸入 y 或 n"
-    
-    while True:
-        ans = raw_input('是否更改\"上課態度\"(預設 優良) (y/n): ')
-        if ans == "y":
-            student['attitude']=raw_input('請輸入上課態度評語: ')
-            print "輸入完成"
-            break
-        if ans == "n":
-            break
-        print "請輸入 y 或 n"
-   
-
-    while True:
-        ans = raw_input('是否更改\"學習狀況\"(預設 優良) (y/n): ')
-        if ans == "y":
-            student['attitude']=raw_input('請輸入學習狀況評語: ')
-            print "輸入完成"
-            break
-        if ans == "n":
-            break
-        print "請輸入 y 或 n"
-   
-
-    msg = msg_template.format(person=student,homework=homework,study=study,specialword=special_word)
+config = ConfigParser.ConfigParser()
+config.read('mail.cfg')
+for section in config.sections():
+    if section in 'DEFAULT':
+        continue
+    student = {'name':section}
+    for attr in ['quiz','study','studycase','attitude','date','homework','specialword','email_list']:
+        student[attr] = config.get(section,attr,vars={})
+    msg = msg_template.format(a = student)
 
     print "Demo"
     print "==================================="
     print msg
     print "==================================="
-    
+    print student['email_list'].split()
     while True:
         ans = raw_input('要寄出嗎 (y/n): ')
         if ans in 'yn':
             break
         print "請輸入 y 或 n"
-    
     if ans in 'n':
         continue
-    
-    mailer = MailSender(student['email'])
+    mailer = MailSender(student['email_list'].split())
+    date = config.get('DEFAULT','date')
     mailer.write_message(msg,date)
     mailer.send()
